4a76250eccd32a5ef1dc7feb7cacac63b6b802d6
Антон Кривохижин <antonkrivohizin@Antons-iMac.local>
2019-05-19 18:06:53 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { TabsComponent } from './tabs/tabs.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tabs';
+
+  @ViewChild(TabsComponent) tabsComponent: TabsComponent;
+
+  user = {
+    name: 'Anton',
+    age: 22
+  }
+
+  @ViewChild('thirdTab') thirdTabTemplate;
+  @ViewChild('customTab') customTabTemplate;
+
+  onFirstTabClick() {
+    this.tabsComponent.openTab('Dynamic Tab', this.thirdTabTemplate, this.user, true);
+  }
+
+  onAnotherTabClick() {
+    this.tabsComponent.openTab('Age Tab', this.customTabTemplate, this.user, true);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tabs/tab.component';
+import { DynamicTabAnchorDirective } from './tabs/dynamic-tab-anchor.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TabComponent,
+    DynamicTabAnchorDirective,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [TabComponent]
 })
 export class AppModule { }
